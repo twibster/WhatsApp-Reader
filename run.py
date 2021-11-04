c282310b351159ccf11f0841605e6856ab7104b5
Omar <omaromran2003@gmail.com>
2021-11-04 06:17:54 +0200

    override default body margins and paddings
    fix uploaded file name floats out of the button

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from website import app
+import socket
+
+ip = socket.gethostbyname(socket.gethostname())
 
 if __name__ =='__main__':
-	app.run(debug =False)
+	app.run(debug =False,host=ip)
--- website/static/scripts/homeMobile.js
+++ website/static/scripts/homeMobile.js
@@ -2,5 +2,5 @@ document.body.style.overflow = "auto";
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 if (isMobile){
 	var down=document.getElementById('down')
-	down.style.width='100%'
+	down.style.width='95%'
 }
