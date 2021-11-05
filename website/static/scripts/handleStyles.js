1c659413577bf2b6ae6fedece8b9a039a98edba0
Omar <omaromran2003@gmail.com>
2021-11-05 04:53:47 +0200

    fix elements overflow out of the header
    this bug caused the sidebar to behave inconsistently

--- website/static/scripts/handleStyles.js
+++ website/static/scripts/handleStyles.js
@@ -8,17 +8,6 @@ function handle_color(card){
 		}
 	}
 }
-
-function hide_convos() {
-	document.getElementById('convos-col').classList.toggle('visible')
-	var header =document.getElementsByClassName('header-convo')[0]
-	if (header.classList.contains('hide-header')){
-		header.classList.toggle('hide-header')
-	}
-
-}
 function alter_side() {
 	document.getElementById('convos-col').classList.toggle('visible')
-	document.getElementsByClassName('header-convo')[0].classList.toggle('hide-header')
-	document.getElementsByClassName('convo-col')[0].classList.toggle('hide-body')
 }
